066c89c8decce2ba8d7d55eaa5ec6ab7f52da00f
Salooma <saloomaali2000@gmail.com>
2023-02-03 11:26:02 +0530

    new project created

---
+++ src/main/java/com/example/FashionAppBackend/FashionAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.FashionAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FashionAppBackend/FashionAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FashionAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
